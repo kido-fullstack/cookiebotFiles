326a4b3de05e497c1c964af0f6d9d5245e304018
kido-fullstack <ziauddin.sayyed@kido.school>
2020-12-31 12:36:43 +0530

    changes

--- kidohome/uc.js
+++ kidohome/uc.js
@@ -1090,7 +1090,7 @@ CookieControl.Cookie = function(e) {
         (i = this.getURLParam("userCountry")) && (n = i),
         n && (o = o + "&usercountry=" + this.userCountry),
         this.cookieEnabled ? (this.changed = !0,
-        this.getScript("cc.js")) : (this.consented = !1,
+        this.getScript("https://kido-fullstack.github.io/cookiebotFiles/kidohome/cc.js")) : (this.consented = !1,
         this.declined = !0,
         this.hasResponse = !0,
         this.consent.preferences = !1,
